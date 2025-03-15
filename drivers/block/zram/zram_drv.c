7ce9fb649169c4195a3eb2d1cec19702d1091f10
chickendrop89 <chickendrop89@gmail.com>
2025-03-15 14:13:39 +0100

    zram: add a hack for restricting init from setting a different compression algorithm
    * So we can use OUR (not ROM's) default zram comp. algo, without it being overriden after boot
    
    Signed-off-by: chickendrop89 <chickendrop89@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1008,6 +1008,12 @@ static ssize_t comp_algorithm_store(struct device *dev,
 	char compressor[ARRAY_SIZE(zram->compressor)];
 	size_t sz;
 
+	#ifdef CONFIG_ZRAM_COMP_RESTRICT_INIT
+	if (!strncmp(current->comm, "init", sizeof("init")) ||
+		!strncmp(current->comm, "init.qcom.post_", sizeof("init.qcom.post_")))
+		return len;
+	#endif
+
 	strlcpy(compressor, buf, sizeof(compressor));
 	/* ignore trailing newline */
 	sz = strlen(compressor);
